2a0e53904ba08998355242a5e1071508502bf7a7
minimimic <asuma.ito198@gmail.com>
2025-02-13 22:18:50 +0900

    added achievement

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-window.addEventListener('load',function(){
-    document.getElementById('mainScreen').style.display = 'none';
-})
+// window.addEventListener('load',function(){
+//     document.getElementById('mainScreen').style.display = 'none';
+// })
 
-document.getElementById('uzuka').addEventListener('animationend', () => {
-    document.getElementById('firstScreen').style.animation = 'hideFirstScreen forwards 2s';
-    setTimeout(function(){
-        document.getElementById('mainScreen').style.display = 'initial';
-        document.getElementById('mainScreen').style.animation = 'showMain forwards 2s';
-    },2000);
-})
+// document.getElementById('uzuka').addEventListener('animationend', () => {
+//     document.getElementById('firstScreen').style.animation = 'hideFirstScreen forwards 2s';
+//     setTimeout(function(){
+//         document.getElementById('mainScreen').style.display = 'initial';
+//         document.getElementById('mainScreen').style.animation = 'showMain forwards 2s';
+//     },2000);
+// })
